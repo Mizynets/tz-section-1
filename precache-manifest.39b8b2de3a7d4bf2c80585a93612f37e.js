4d04179b75c5c887de7c387df17198d2ee2f04ed
Sanchez <smizinec@gmail.com>
2019-09-29 13:08:41 +0300

    Updates

---
+++ precache-manifest.39b8b2de3a7d4bf2c80585a93612f37e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05b1f7d6c50ab5c252da15b730195a9",
+    "url": "/Mizynets/tz-section-1/index.html"
+  },
+  {
+    "revision": "1fc3379577905616bcdb",
+    "url": "/Mizynets/tz-section-1/static/css/main.a44999e3.chunk.css"
+  },
+  {
+    "revision": "70195d67ae6af8d70db6",
+    "url": "/Mizynets/tz-section-1/static/js/2.dca676fd.chunk.js"
+  },
+  {
+    "revision": "1fc3379577905616bcdb",
+    "url": "/Mizynets/tz-section-1/static/js/main.ad082e53.chunk.js"
+  },
+  {
+    "revision": "0f6d59dd5614ea5b3c31",
+    "url": "/Mizynets/tz-section-1/static/js/runtime-main.5605559c.js"
+  },
+  {
+    "revision": "17dfdbbf5bdccdaaab750f16534b3fd6",
+    "url": "/Mizynets/tz-section-1/static/media/graphOrderStatistic.17dfdbbf.png"
+  },
+  {
+    "revision": "962d0d1e80bb1c780e9c1a4b4dbe09c5",
+    "url": "/Mizynets/tz-section-1/static/media/transactionMap.962d0d1e.png"
+  }
+]);
