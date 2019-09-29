3da338fafa4503e7339c23c4b79784701922e07d
Sanchez <smizinec@gmail.com>
2019-09-29 13:12:09 +0300

    Updates

---
+++ precache-manifest.e9e52bbdd81ff91cefa96eb2c563dd39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c555049c2e8e79e47444c95d27f10cd7",
+    "url": "/tz-section-1/index.html"
+  },
+  {
+    "revision": "f3786e4d343f05833122",
+    "url": "/tz-section-1/static/css/main.a44999e3.chunk.css"
+  },
+  {
+    "revision": "70195d67ae6af8d70db6",
+    "url": "/tz-section-1/static/js/2.dca676fd.chunk.js"
+  },
+  {
+    "revision": "f3786e4d343f05833122",
+    "url": "/tz-section-1/static/js/main.21e08bce.chunk.js"
+  },
+  {
+    "revision": "dd6d4ee63f439631b0a1",
+    "url": "/tz-section-1/static/js/runtime-main.f6967f84.js"
+  },
+  {
+    "revision": "17dfdbbf5bdccdaaab750f16534b3fd6",
+    "url": "/tz-section-1/static/media/graphOrderStatistic.17dfdbbf.png"
+  },
+  {
+    "revision": "962d0d1e80bb1c780e9c1a4b4dbe09c5",
+    "url": "/tz-section-1/static/media/transactionMap.962d0d1e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mizynets/tz-section-1/precache-manifest.39b8b2de3a7d4bf2c80585a93612f37e.js"
+  "/tz-section-1/precache-manifest.e9e52bbdd81ff91cefa96eb2c563dd39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mizynets/tz-section-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tz-section-1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
